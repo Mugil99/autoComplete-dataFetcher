2abe1694ae311b547ae65205211c06ceb9abc742
Mugil99 <mugilya99@gmail.com>
2023-06-20 21:21:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import DataFetcher from './Components/Datafetcher';
+const data = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 function App() {
+const[fruit,setFruit]=useState("");
+const[arr,setArr]=useState(data);
+const str="hi my name is mugil \n im 23 years old"
+useEffect(()=>{
+    var a=[...data];
+    var newArr=a.filter((item)=>item.includes(fruit));
+    setArr(newArr);
+},[fruit])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search item</h1>
+       <input type="text" onChange={(e)=>setFruit(e.target.value)}></input>
+        <ul>
+          {
+            arr.map((item,index)=>(
+              <li>{item}</li>
+            ))
+        }
+        </ul>
+        <DataFetcher/>
+        <pre>{str}</pre>
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        {/* just to learn about pre tag */}
     </div>
   );
 }
---
+++ src/Components/Datafetcher.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const DataFetcher = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default DataFetcher;
